8016f00eb16d9bc8117c0db47495fd6027036634
Carlos Brazil <cabrazil@gmail.com>
2020-08-19 21:59:33 -0300

    Repositories crud complete + likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
+const { response } = require("express");
 
 const app = express();
 
@@ -11,23 +12,93 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  // Lista os repositórios
+  return response.json(repositories);  
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  // Cria um repositório com likes = 0
+  const { title, url, techs} = request.body;
+
+  const repositoy = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repositoy);
+  return response.json(repositoy);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  // Obter o ID e demais campos
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  // Encontrar o respositório solicitado
+  const repoIndex = repositories.findIndex(repository => repository.id === id);
+
+  // Se não encontrar, exibe status
+  if (repoIndex < 0) {
+        return response.status(400).send();
+  }
+
+  // Atualiza repositório, mas mantém valor original de likes
+  const likes = repositories[repoIndex].likes;
+  const repository = {
+      id,
+      title,
+      url,
+      techs,
+      likes
+  };
+
+  repositories[repoIndex] = repository;
+  return response.json(repository);
+
+  // Devolver status de sucesso
+  return response.status(204).send();
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  // Obter o ID
+  const { id } = request.params;
+
+  // Encontrar o respositório solicitado
+  const repoIndex = repositories.findIndex(repository => repository.id === id);
+
+  // Se não encontrar, exibe status
+  if (repoIndex < 0) {
+        return response.status(400).send();
+  }
+
+  // Excluir o repositório encontrado
+  repositories.splice(repoIndex, 1);
+
+  // Devolver status de sucesso
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  // Obter o Id
+  const { id } = request.params;
+  
+  // Encontrar o repositório para like
+  const repository = repositories.find(repository => repository.id === id);
+
+  // Se não encontrar, exibe status
+  if (!repository) {
+        return response.status(400).send();
+  }
+
+  // Adiciona like
+  repository.likes++;
+  return response.json(repository);
+
 });
 
 module.exports = app;
